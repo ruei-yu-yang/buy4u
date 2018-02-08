822d1256a4378194780243bddfaf918f56e9fcb2
Ruei Yu <vip8209@yahoo.com.tw>
2018-02-08 15:26:10 +0800

    Implementation takkephoto.

--- app/src/main/java/com/rueiyu/buy4u/AddActivity.java
+++ app/src/main/java/com/rueiyu/buy4u/AddActivity.java
@@ -1,6 +1,8 @@
 package com.rueiyu.buy4u;
 
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +11,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.jph.takephoto.app.TakePhoto;
+import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.model.CropOptions;
+import com.jph.takephoto.model.TResult;
+import com.jph.takephoto.model.TakePhotoOptions;
 import com.rueiyu.buy4u.databinding.ActivityAddBinding;
 
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends TakePhotoActivity {
 
     private ImageView mPhoto;
     private TextView mPrevName;
@@ -27,6 +36,7 @@ public class AddActivity extends AppCompatActivity {
     private EditText mQty;
     private EditText mName;
     private ActivityAddBinding binding;
+    private String mImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +64,13 @@ public class AddActivity extends AppCompatActivity {
         mQty = findViewById(R.id.qty);
         mName = findViewById(R.id.name);
 
+        mSelectImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickPhoto();
+            }
+        });
+
         mBtnDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,6 +86,7 @@ public class AddActivity extends AppCompatActivity {
                 }
                 item.setPrice(Integer.parseInt(mPrice.getText().toString()));
                 item.setQty(Integer.parseInt(mQty.getText().toString()));
+                item.setPhotoPath(mImagePath);
 
                 new Thread(new Runnable() {
                     @Override
@@ -79,5 +97,48 @@ public class AddActivity extends AppCompatActivity {
                 }).start();
             }
         });
+    }
+
+    private void pickPhoto() {
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "/buy4u/" + System.currentTimeMillis()+".jpg");
+        if (!file.getParentFile().exists()){
+            file.getParentFile().mkdirs();
+        }
+        Uri uri = Uri.fromFile(file);
+        TakePhoto takePhoto = getTakePhoto();
+        takePhoto.setTakePhotoOptions(new TakePhotoOptions.Builder()
+                .setWithOwnGallery(true)
+                .create());
+        takePhoto.onPickFromGalleryWithCrop(uri, getCropOptions());
+    }
+
+    private CropOptions getCropOptions() {
+        int width = 600;
+        int height = 720;
+        CropOptions.Builder builder = new CropOptions.Builder()
+                .setAspectX(width)
+                .setAspectY(height)
+                .setWithOwnCrop(true);
+        return builder.create();
+    }
+
+    @Override
+    public void takeSuccess(TResult result) {
+        super.takeSuccess(result);
+        mImagePath = result.getImage().getOriginalPath();
+        Glide.with(this)
+                .load(mImagePath)
+                .into(mPhoto);
+    }
+
+    @Override
+    public void takeFail(TResult result, String msg) {
+        super.takeFail(result, msg);
+    }
+
+    @Override
+    public void takeCancel() {
+        super.takeCancel();
     }
 }
